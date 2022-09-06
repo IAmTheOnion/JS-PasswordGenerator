9f917e7f76d19dcfd09e82282507be7401c82148
Ferdinand Lame <87307561+IAmTheOnion@users.noreply.github.com>
2022-09-06 09:58:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 $( document ).ready(function() {
     $(document).on('input', '#slider', function() {
-        console.log("Test");
+        document.getElementById("password").value = randomPassword(this.value, document.getElementById("cl").value, document.getElementById("sc").value);
     });
+
+
 });
 
 console.log("test");
+
+function randomPassword(l, cl, sc) {
+
+    let code = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let final = "";
+
+    if (cl == "on") {
+        code += "ABCDEFGHJIKLMNOPQRSTUVWXYZ";
+    }
+
+    if (sc == "on") {
+        code += "!@#$%:&+=";
+    }
+
+    for(let i = 0; i < l; i++) {
+        final += code.charAt(Math.floor(Math.random() * code.length));
+    }
+
+    return final;
+}
